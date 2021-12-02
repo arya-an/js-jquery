fdbfedcec49da6ea3e911df23d89745e8aaa22cc
arya-an <aryaanand1298@gmail.com>
2021-12-02 11:07:02 +0530

    jquery basic events

---
+++ js/event.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  
+    // function fnalert(){
+    //     alert('welcome')
+    // }
+    // $('#btnlogin').click(fnalert);
+    $('#btnlogin').click(function(){   //for double click dblclick,keypress
+
+    
+        var uname=$('#username').val();
+        var passw=$('#password').val();
+        if (uname==""){
+            alert('enter uname');
+        }
+        else if (passw==""){
+            alert('enter password');
+        }
+        else{
+            alert('welcome');
+        }
+    });
+
+});
